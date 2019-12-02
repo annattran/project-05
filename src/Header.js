b46f745e478b275aada3ccc6e40704e0a23183bd
Anna <annatran93@gmail.com>
2019-12-01 22:28:38 -0500

    styled header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
+import leaf from './assets/leaf-01.png';
+import foil from './assets/foil.jpg';
 
 
 class Header extends Component {
@@ -16,9 +18,18 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <h1>Jack & Jill</h1>
-                <h2>November 28th 2019</h2>
-                <button onClick={this.onClick}> Add Comment</button>
+                <img src={leaf} className="left leaf" />
+                <img src={leaf} className="right leaf" />
+                <div className="button">
+                    <button onClick={this.onClick}> Add Comment</button>
+                </div>
+                <div className="title">
+                    <h1>Kelvin ❤ Karen</h1>
+                    <h2>November 28th 2019</h2>
+                    <div className="instructions">
+                        <p>Record a video with your well wishes, something funny, words of advice, or maybe date night ideas. When we look back we'll be reminded of all the people who helped celebrate our special day.</p>
+                    </div>
+                </div>
             </header>
         )
     }
