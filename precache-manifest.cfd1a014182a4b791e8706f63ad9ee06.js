4620f3bbbe1b91443904439593fe413034d7d361
Anna <annatran93@gmail.com>
2019-12-03 11:02:21 -0500

    Updates

---
+++ precache-manifest.cfd1a014182a4b791e8706f63ad9ee06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6deb48e826dad119aa467849e397d2d2",
+    "url": "/project-05-anna-tran/index.html"
+  },
+  {
+    "revision": "0fdf9a88cb6cdc721074",
+    "url": "/project-05-anna-tran/static/css/2.ba38f8e4.chunk.css"
+  },
+  {
+    "revision": "2ff7819864d0c3ae7b33",
+    "url": "/project-05-anna-tran/static/css/main.2939f246.chunk.css"
+  },
+  {
+    "revision": "0fdf9a88cb6cdc721074",
+    "url": "/project-05-anna-tran/static/js/2.b6ec7c75.chunk.js"
+  },
+  {
+    "revision": "2ff7819864d0c3ae7b33",
+    "url": "/project-05-anna-tran/static/js/main.29116f22.chunk.js"
+  },
+  {
+    "revision": "8360f8356b31a584eae2",
+    "url": "/project-05-anna-tran/static/js/runtime-main.1a95aaea.js"
+  },
+  {
+    "revision": "abdb6236d5b7f9a5687c74128047c5c2",
+    "url": "/project-05-anna-tran/static/media/foil.abdb6236.jpg"
+  },
+  {
+    "revision": "59ff8a6c836b52bf3db1a1951ca58869",
+    "url": "/project-05-anna-tran/static/media/leaf.59ff8a6c.png"
+  }
+]);
